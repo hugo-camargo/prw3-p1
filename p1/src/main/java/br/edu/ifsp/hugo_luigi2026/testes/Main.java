efc054f9b130299703ffbe1ea9f4b9da4905b1a4
Luigi <a.luigi@aluno.ifsp.edu.br>
2026-02-23 17:24:57 -0300

    busca pelo nome do aluno feita

--- p1/src/main/java/br/edu/ifsp/hugo_luigi2026/testes/Main.java
+++ p1/src/main/java/br/edu/ifsp/hugo_luigi2026/testes/Main.java
@@ -6,12 +6,9 @@ import br.edu.ifsp.hugo_luigi2026.util.JPAUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 
-import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
